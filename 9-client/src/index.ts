90ecbe42debffa9d719e18290c7698fd8d7a6b66
Maxim Van de Wynckel <maxim.van.de.wynckel@vub.be>
2022-02-01 17:25:02 +0100

    wip: socket response, work in progress

---
+++ 9-client/src/index.ts
@@ -0,0 +1,34 @@
+import { DataFrame, DataObject, Model, ModelBuilder } from '@openhps/core';
+import { SocketClient, SocketClientSink } from '@openhps/socket';
+import { RelativeRSSI, BLEObject } from '@openhps/rf';
+
+console.log("Creating the positioning model ...");
+
+ModelBuilder.create()
+    .withLogger(console.log)            // Optional: useful with troubleshooting
+    // Create a socket client. One positioning model might have a connection to a server
+    // with multiple endpoints.
+    .addService(new SocketClient({
+        url: "http://localhost:3000",
+        path: "/api/v1"
+    }))
+    // Empty source, we manually push data
+    .from()
+    // Socket sink, data is transmitted to a server on port 3000
+    .to(new SocketClientSink({
+        uid: "online"
+    }))
+    .build().then((model: Model) => {
+        console.log("Client positioning model created ...");
+
+        setTimeout(() => {
+            const dataFrame = new DataFrame();
+            dataFrame.source = new DataObject("mvdewync", "Maxim Van de Wynckel");
+            // Example has two relative beacons with same RSSI (so the position should be in the middle if the calibration data is the same)
+            dataFrame.source.addRelativePosition(new RelativeRSSI(new BLEObject("5DC48FBFB912"), -50));
+            dataFrame.source.addRelativePosition(new RelativeRSSI(new BLEObject("3E182D702D4C"), -50));
+            
+            console.log("Sending data frame ...");
+            model.push(dataFrame);
+        }, 500);
+    }).catch(console.error);
